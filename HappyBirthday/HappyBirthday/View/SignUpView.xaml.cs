c8b2233d5274e871d131a95e6fcf627f983674e8
Un1VersE <gamaundavid23@gmail.com>
2019-03-15 21:45:09 +0200

    Произошел Си хештег

---
+++ HappyBirthday/HappyBirthday/View/SignUpView.xaml.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Windows.Controls;
+using HappyBirthday.ViewModel;
+
+
+namespace HappyBirthday.View
+{
+    /// <summary>
+    /// Interaction logic for SignUpView.xaml
+    /// </summary>
+    public partial class SignUpView : UserControl
+    {
+        public SignUpView()
+        {
+            InitializeComponent();
+            DataContext = new SignUpViewModel();
+        }
+    }
+}
